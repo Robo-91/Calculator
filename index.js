2f0279921043761fe26205eeacf336cd963a6d9d
Robert Palmer <r91palmer@gmail.com>
2020-04-30 23:00:53 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const container = document.getElementById('container1');
+const clear = document.getElementById('clear');
+const backspace = document.getElementById('backspace');
+const equal = document.getElementById("equal");
+let display = document.getElementById('display');
+let content = document.getElementById('content');
+let numOne = '';
+let operateSign = [];
+let numTwo = '';
+let result;
+let displayValue = [];
+
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
+
+function operate(a = parseFloat(numOne),operator, b = parseFloat(numTwo)) {
+
+	operator = operateSign;
+
+	if(operator === `+`) {
+		result = add(a,b);
+	}
+
+	if(operator === `-`) {
+		result = subtract(a,b);
+	}
+
+	if(operator === `*`) {
+		result = multiply(a,b);
+	} 
+
+	if(operator === `/`) {
+		result = divide(a,b);
+	}
+	numOne = "";
+	operateSign = "";
+	numTwo = "";
+	displayValue = [];
+	console.log(a);
+	console.log(operator);
+	console.log(b);
+	// result = result.fixed(10);
+	return result;
+
+}	
+
+container.addEventListener("click",function(e) {
+
+	if(e.target.textContent !== "C" && e.target.textContent !== "<-" && e.target.textContent !== "="){
+		displayValue.push(e.target.textContent);
+		content.textContent = displayValue.join('');
+
+		if(e.target.className === "number" && !numTwo.length && !operateSign.length){
+			numOne = display.textContent;
+		}
+		
+		if(e.target.className === "operator" && numOne.length && !numTwo.length){
+			operateSign = e.target.textContent;
+		}
+
+		if(numOne.length && operateSign.length){
+			numTwo = displayValue.slice(displayValue.indexOf(operateSign) + 1);
+			numTwo = numTwo.join('');
+		}
+	}
+
+	if(numOne.length && operateSign.length && numTwo.length && e.target.className === 'operator'){
+		operate();
+		numOne = result.toString();
+		operateSign = e.target.textContent;
+		content.textContent = result + e.target.textContent;
+		displayValue.push(result.toString(),operateSign);
+		numTwo = displayValue.slice(displayValue.indexOf(operateSign) + 1);
+		numTwo = numTwo.join('');
+	}
+
+	if(e.target.id === "container1"){
+		displayValue.pop(e.target.textContent);
+		content.textContent = displayValue.join('');
+	}
+});
+
+clear.addEventListener("click", function(){
+	displayValue = [];
+	content.textContent = "";
+	numOne = "";
+	operateSign = "";
+	numTwo = "";
+});
+
+backspace.addEventListener("click", function(){
+	displayValue.pop();
+	content.textContent = displayValue.join('');
+});
+
+equal.addEventListener("click", function(){
+	operate();
+	content.textContent = result;
+
+	if(operateSign === "/" && numTwo === '0'){
+		content.textContent = "Invalid operation!";
+	}
+
+	if(result === NaN){
+		content.textContent = "Invalid operation!";
+	}
+});
+
